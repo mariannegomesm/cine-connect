99139f64ff133ce618669b2fe0d3b86cc76055f2
Pedro Mendonça <pedrolucasmacm@gmail.com>
2024-12-03 04:09:21 -0300

    Adicionando logica para busca de filmes

--- app/src/main/java/com/example/cine_connect/data/models/Movie.kt
+++ app/src/main/java/com/example/cine_connect/data/models/Movie.kt
@@ -2,4 +2,9 @@ package com.example.cine_connect.models
 
 
 
-data class Movie(val id: Int, val posterPath: String)
+data class Movie(
+    val id: Int,
+    val posterPath: String,
+    val movieTitle: String,
+    val overview: String) {
+}
